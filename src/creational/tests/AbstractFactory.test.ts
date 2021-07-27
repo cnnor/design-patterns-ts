7a9d15a208b94f2f18e0717304d9c06a6cac65b8
Connor <cnnor@users.noreply.github.com>
2021-07-27 10:15:08 -0700

    feat: add creational abstract factory pattern

---
+++ src/creational/tests/AbstractFactory.test.ts
@@ -0,0 +1,38 @@
+import {
+  FactoryMaker,
+  HumanArcher,
+  HumanMage,
+  HumanTeamFactory,
+  HumanWarrior,
+  TeamType,
+} from '../AbstractFactory';
+
+test('factory maker returns a human team factory', () => {
+  const humanFactory = FactoryMaker.makeFactory(TeamType.HUMAN);
+  expect(humanFactory).toBeInstanceOf(HumanTeamFactory);
+});
+
+test('human factory returns a human team', () => {
+  const humanFactory = FactoryMaker.makeFactory(TeamType.HUMAN);
+  const archer = humanFactory.createArcher();
+  const warrior = humanFactory.createWarrior();
+  const mage = humanFactory.createMage();
+  const team = { archer, warrior, mage };
+
+  expect(team).toMatchObject({
+    archer: new HumanArcher(),
+    warrior: new HumanWarrior(),
+    mage: new HumanMage(),
+  });
+});
+
+test('humans return description', () => {
+  const humanFactory = FactoryMaker.makeFactory(TeamType.HUMAN);
+  const archer = humanFactory.createArcher();
+  const warrior = humanFactory.createWarrior();
+  const mage = humanFactory.createMage();
+
+  expect(archer.getDescription()).toBe('An archer of the human species.');
+  expect(warrior.getDescription()).toBe('A warrior of the human species.');
+  expect(mage.getDescription()).toBe('A mage of the human species.');
+});
